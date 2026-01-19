b865a6d2b33a09c4ae7d301f417157a2aa1acdb2
nandeeshb7 <nandeeshb7>
2026-01-19 21:21:00 +0530

    fixed errors in root

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,12 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "../../../db/sample-data";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
 
-const delay = (ms:any) => new Promise((resolve) =>setTimeout(resolve,ms))
-console.log(sampleData)
+export const dynamic = 'force-dynamic'
 
  const HomePage = async () => {
 
   const latestProducts = await getLatestProducts();
-  await delay(2000)
 
 
   return (
